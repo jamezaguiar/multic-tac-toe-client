2076b65cb087a0ba06e86597d6ed441f159165fe
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-22 11:08:32 -0300

    Configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Multic-tac-toe</h1>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/Game/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Game: React.FC = () => {
+  return <h1>Game</h1>;
+};
+
+export default Game;
